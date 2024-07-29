1d9fce2bf272a1eb34bf3af2b02b3e6590e61671
FiorellaCrocco <fiorellacrocco@gmail.com>
2024-07-29 20:34:41 -0300

    agrego contenido de etiquetas

--- clase3/profe/main.go
+++ clase3/profe/main.go
@@ -1,17 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
+// Para usar etiquetas no debe haber espacios entre `json:""`
 type Person struct {
-	Id          int
-	Name        string
-	DateOfBirth string
+	Id          int    `json:"id"`
+	Name        string `json:"name,omitempty"` // Usamos "omitempty" para que si no se pasa el valor al instanciar se omita
+	DateOfBirth string `json:"dateofbirth"`
 }
 
 type Employee struct {
-	Id       int
-	Position string
-	Person   Person
+	Id       int    `json:"-"` // El "-" se usa para no mostrar ese valor, es decir, ocultarlo
+	Position string `json:"position"`
+	Person   Person `json:"person"`
 }
 
 // metodo asociado a employee, estructura que devuelve string
@@ -29,7 +34,34 @@ func main() {
 	e1 := Employee{1, "Gerente", p1}
 
 	//Llamamos al método que retorna string
+	fmt.Println("\n ")
+	fmt.Print("PrintEmployee en string: \n")
 	fmt.Println(e1.PrintEmployee())
 	// Llamamos al método que no le asignamos que devuelve (no usamos return)
+	fmt.Println("\n ")
+	fmt.Print("PrintEmployee sin return: \n")
 	e1.PrintEmployee2()
+
+	// Código de ejemplo para ver el uso de etiquetas, convirtiendo a formato json (Para eso uso json.Marchal)
+	employee, err := json.Marshal(e1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("\n ")
+	fmt.Print("PrintEmployee con Marshal: \n")
+	fmt.Println(string(employee))
+
+	// Código de ejemplo para convertir de json a string (formato definido en PrintEmployee, para eso uso json.Unmarchal)
+	var e2 Employee
+
+	employee2 := `{"Id":1,"Position":"Gerente","Person":{"Id":1,"Name":"Juan","DateOfBirth":"10/10/2000"}}`
+
+	if err2 := json.Unmarshal([]byte(employee2), &e2); err2 != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("\n ")
+	fmt.Print("PrintEmployee con Unmarchal: \n")
+	fmt.Println(e2.PrintEmployee())
 }
